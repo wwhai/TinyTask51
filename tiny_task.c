b799dab4458056bfee877e4389b916b78fbf707f
wwhai <cnwwhai@gmail.com>
2023-01-10 00:09:25 +0800

    dev: add some apis

--- tiny_task.c
+++ tiny_task.c
@@ -1,5 +1,6 @@
 #include <reg51.h>
 #include "tiny_task.h"
+
 /// @brief 初始化T0，而T0就是我们的TinyTask调度要用到的定时器
 /// @param
 void T0Init(void)
@@ -11,6 +12,87 @@ void T0Init(void)
     TR0  = 1;
     ET0  = 1;
 }
+/// @brief 初始化
+/// @param
+void TT_init(TinyTask *tt)
+{
+    T0Init();
+    tt->current_task_id = 0;
+}
+/// @brief 启动OS
+/// @param tt
+void TT_start(TinyTask *tt)
+{
+    // Task0 表示系统进程
+    if (tt->tasks[0].task_control.state == READY) {
+        SP = tt->tasks[0].contet_sp;
+    }
+}
+/// @brief  停止
+/// @param tt
+void TT_stop(TinyTask *tt)
+{
+    unsigned char i;
+    for (i = 0; i < MAX_TASKS; i++) {
+        if (tt->tasks[i].task_control.valid) {
+            tt->tasks[i].task_control.valid = 0;
+        }
+        if (tt->tasks[i].time_slice > 0) {
+            tt->tasks[i].time_slice = -1;
+        }
+    }
+}
+
+/// @brief 执行任务
+/// @param task
+void TT_go(TinyTask *tt, Task task)
+{
+    unsigned char i;
+    for (i = 0; i < MAX_TASKS; i++) {
+        if (!tt->tasks[i].task_control.valid) {
+            tt->tasks[i] = task;
+        }
+    }
+}
+
+/// @brief 取消一个任务
+/// @param tt
+/// @param task_id
+void TT_cancel(TinyTask *tt, unsigned char task_id)
+{
+    if (tt->tasks[task_id].task_control.valid) {
+        tt->tasks[task_id].task_control.valid = 0;
+    }
+    if (tt->tasks[task_id].time_slice > 0) {
+        tt->tasks[task_id].time_slice = -1;
+    }
+    if (tt->tasks[task_id].sleep_time > 0) {
+        tt->tasks[task_id].sleep_time = -1;
+    }
+}
+
+/// @brief 主动让出CPU
+/// @param tt
+void TT_yield(TinyTask *tt)
+{
+    tt->tasks[tt->current_task_id].contet_sp = SP; // 保存上下文
+    if (tt->current_task_id + 1 > MAX_TASKS) {
+        tt->current_task_id = 0; // 说明到底了, 回到第一个任务
+    }
+    // 此处应该处理优先级, 不过目前暂时先实现最简单的切换效果
+    // --> 优先级 MIN = 排序任务表()
+    // --> yield-to-min(SP = tt->tasks[优先级最小的].contet_sp)
+    SP = tt->tasks[tt->current_task_id + 1].contet_sp;
+}
+
+/// @brief 任务表
+/// @param tt
+void TT_task_list(TinyTask *tt)
+{
+    // printf("--------------------------")
+    // printf("--------------------------")
+}
+
 /// @brief T0中断函数
 
 void T0_IRQ_Handler() interrupt 1
@@ -18,9 +100,35 @@ void T0_IRQ_Handler() interrupt 1
     TL0 = 0x00; // 设置定时初始值: 5ms
     TH0 = 0x28; // 设置定时初始值: 5ms
 }
-
+///
+void t0f(void *args)
+{
+    while (1)
+        ;
+}
+///
+void t1f(void *args)
+{
+}
+void t2f(void *args)
+{
+}
 /// @brief Main
+TinyTask tt;
 void main()
 {
-    T0Init();
+    TT_init(&tt);
+    // task1
+    Task t0;
+    t0.id                     = 0;
+    t0.contet_sp              = 0;
+    t0.time_slice             = 100;
+    t0.sleep_time             = 0;
+    t0.task_control.state     = READY;
+    t0.task_control.valid     = 1;
+    t0.task_control.task_type = CALCULATE;
+    t0.task_control.priority  = 0;
+    t0.func                   = t0f;
+    TT_go(&tt, t0);
+    TT_start(&tt);
 }
--- tiny_task.h
+++ tiny_task.h
@@ -1,6 +1,7 @@
 #ifndef _TINY_TASK_H_
 #define _TINY_TASK_H_
 
+#define MAX_TASKS 16
 typedef unsigned char uint8_t;
 
 /// @brief 任务状态
@@ -20,7 +21,7 @@ typedef struct TaskControl {
     volatile uint8_t valid : 1;    // 是否是有效任务
     volatile uint8_t switched : 1; // 是否已经被保存了上下文
     TaskType task_type : 2;        // 任务的类型, 0:IO; 1: Calculate
-    volatile uint8_t priority : 4; // 任务的优先级 0-16, 越小越优先
+    volatile uint8_t priority : 4; // 任务的优先级 0-16, 越小越优先,0 表示不需要优先级
     TaskState state;               // 任务的当前状态
 } TaskControl;
 
@@ -29,40 +30,44 @@ typedef void (*TaskFunc)(void *args);
 /// @brief 任务定义
 typedef struct {
     uint8_t id;                        // ID => [0,16]
+    unsigned short contet_sp;          // 保存的上下文SP纸箱的 21个寄存器的首地址
     volatile TaskControl task_control; // 任务控制参数
-    volatile uint8_t time_slice;       // 分配给任务的时间片，默认一个任务时间片为100毫秒
-    volatile unsigned long sleep_time; // 假设给sleep了，这里表示sleep的时间
+    volatile char time_slice;          // 分配给任务的时间片，默认一个任务时间片为100毫秒
+    volatile long sleep_time;          // 假设给sleep了，这里表示sleep的时间
     TaskFunc func;                     // 任务执行函数
-    uint8_t local_mailbox[16];         // 本地邮箱, 本质上会被设计成一个消息队列, 用来做进程间通信
+    char local_mailbox[2];             // 本地邮箱, 本质上会被设计成一个消息队列, 用来做进程间通信, -1 表示无消息
 } Task;
 /// @brief RTOS实例
 typedef struct TinyTask {
-    Task tasks[16]; // 任务列表
+    Task tasks[2];           // 任务列表
+    uint8_t current_task_id; // 当前执行的任务ID
 } TinyTask;
 
 /// @brief 初始化
 /// @param
-TinyTask TT_init(void);
-
+void TT_init(TinyTask *tt);
+/// @brief 启动RTOS
+/// @param tt
+void TT_start(TinyTask *tt);
 /// @brief  停止
 /// @param tt
-void TT_stop(TinyTask tt);
+void TT_stop(TinyTask *tt);
 
 /// @brief 执行任务
 /// @param task
-void TT_go(TinyTask tt, Task task);
+void TT_go(TinyTask *tt, Task task);
 
 /// @brief 取消一个任务
 /// @param tt
 /// @param task_id
-void TT_cancel(TinyTask tt, unsigned char task_id);
+void TT_cancel(TinyTask *tt, unsigned char task_id);
 
 /// @brief 主动让出CPU
 /// @param tt
-void TT_yield(TinyTask tt);
+void TT_yield(TinyTask *tt);
 
 /// @brief 任务表
 /// @param tt
-Task *TT_task_list(TinyTask tt);
+void TT_task_list(TinyTask *tt);
 
 #endif
